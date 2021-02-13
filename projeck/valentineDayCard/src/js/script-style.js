89aa28a59b02127334f362d1cb4313b1a0d206aa
AdiSan103 <69831671+AdiSan103@users.noreply.github.com>
2021-02-13 22:37:30 +0800

    updated projek-valentine card
    updated projek-valentine card
    13/02/2021

---
+++ projeck/valentineDayCard/src/js/script-style.js
@@ -0,0 +1,70 @@
+// load
+let t1 = document.getElementsByClassName('text')[0];
+let container = document.getElementsByClassName('container')[0];
+let load = document.getElementsByClassName('load')[0];
+// component
+let cp1 = document.getElementsByClassName('cp1')[0];
+let cp2 = document.getElementsByClassName('cp2')[0];
+let cp3 = document.getElementsByClassName('cp3')[0];
+let cp4 = document.getElementsByClassName('cp4')[0];
+let cp5 = document.getElementsByClassName('cp5')[0];
+let butterfly = document.getElementsByClassName('butterfly')[0];
+let bee = document.getElementsByClassName('bee')[0];
+// leaf-left
+let leafLeft1 = document.getElementsByClassName('leaf-left')[0];
+let leafLeft2 = document.getElementsByClassName('leaf-left')[1];
+let flowerLeft = document.getElementsByClassName('flower-left')[0];
+// leaf-right
+let leafRight1 = document.getElementsByClassName('leaf-right')[0];
+let leafRight2 = document.getElementsByClassName('leaf-right')[1];
+let flowerRight = document.getElementsByClassName('flower-right')[0];
+// content2
+let content2 = document.getElementsByClassName('content2')[0];
+// content3
+let content3 = document.getElementsByClassName('content3')[0];
+// content4
+let content4 = document.getElementsByClassName('content4')[0];
+
+// 1
+setTimeout(function() {
+    t1.innerHTML = "14 Februari 2020";
+    t1.classList.add('true');
+    endLoad();
+}, 5200);
+
+// 2
+function endLoad() {
+    setTimeout(function() {
+        container.classList.add('true');
+        load.classList.add('false');
+        // component
+        setTimeout(function() {
+            component();
+        }, 1000)
+    }, 5000);
+}
+
+// 3
+function component() {
+    cp1.classList.add('true');
+    cp2.classList.add('true');
+    cp3.classList.add('true');
+    cp4.classList.add('true');
+    cp5.classList.add('true');
+    butterfly.classList.add('true');
+    bee.classList.add('true');
+    // leaf-left
+    leafLeft1.classList.add('true');
+    leafLeft2.classList.add('true');
+    flowerLeft.classList.add('true');
+    // leaf-right
+    leafRight1.classList.add('true');
+    leafRight2.classList.add('true');
+    flowerRight.classList.add('true');
+    // content
+    content2.classList.add('true');
+    // content3
+    content3.classList.add('true');
+    // content4
+    content4.classList.add('true');
+}
---
+++ projeck/valentineDayCard/src/js/script-system.js
@@ -0,0 +1,102 @@
+// get value from input sender
+let allDataUrl = window.location.search;
+let getDataUrl = new URLSearchParams(allDataUrl);
+let receiverUrl = getDataUrl.get('HJkaL');
+let messageUrl = getDataUrl.get('MGJiK');
+let senderUrl = getDataUrl.get('LpOUI');
+let copy = document.getElementsByClassName('copy')[1];
+
+// make url
+if(copy) {
+    copy.addEventListener('click', function() {
+        copy.classList.remove('salin');
+        copyData();
+    });
+}
+
+// 1.1
+function copyData() {
+    let receiverCopy = document.getElementsByClassName('receiverCopy')[0];
+    let messageCopy = document.getElementsByClassName('messageCopy')[0];
+    let senderCopy = document.getElementsByClassName('senderCopy')[0];
+    // get data from main form
+    receiverCopy.value = document.getElementById('HJkaL').value;
+    messageCopy.value = document.getElementById('MGJiK').value;
+    senderCopy.value = document.getElementById('LpOUI').value;
+    // if
+    let make = document.getElementsByClassName('salin')[0];
+    if(make) {
+        baseUrl();
+    }
+}
+
+
+// default
+if(receiverUrl === null && senderUrl === null) {        
+    if(messageUrl === null) {
+        copy.classList.remove('salin');
+    }
+}
+
+
+// make url
+    if(receiverUrl || senderUrl) {        
+        if(messageUrl) {
+            // back page
+                if(receiverUrl == "false" && senderUrl == "false") {        
+                    // if(messageUrl == "false") {
+                    //     copy.classList.remove('salin');
+                    //     window.location.href = 'http://127.0.0.1:5500/projeck/valentineDayCard/';
+                    // }
+                } else {
+                    if(copy) {
+                        copy.innerHTML = "Salin link";
+                        copy.classList.add('salin');
+                    }
+                }
+        }
+    }
+
+// copy url
+if(document.getElementsByClassName('salin')[0]) {
+    let make = document.getElementsByClassName('salin')[0];
+    make.addEventListener('click', function() {
+        baseUrl();
+    })
+}
+
+// 2.1s
+function baseUrl() {
+    let urlBase = document.getElementById('baseUrl');
+    urlBase.value = 'https://adisan103.github.io/projeck/xatemp.html?' + getDataUrl;
+    copyUrl();
+}
+// 2.2
+function copyUrl() {
+    copy.classList.add('back');
+    let urlBase = document.getElementById('baseUrl');
+    urlBase.select();
+    document.execCommand("copy");
+    alert("Text berhasil dicopy");
+    let receiverCopy = document.getElementsByClassName('receiverCopy')[0];
+    let messageCopy = document.getElementsByClassName('messageCopy')[0];
+    let senderCopy = document.getElementsByClassName('senderCopy')[0];
+    // post data to url
+    // receiverCopy.value = 'false';
+    // messageCopy.value = 'false';
+    // senderCopy.value = 'false';
+}
+
+
+// output
+let receiverValue = document.getElementsByClassName('receiverData')[0]; 
+// jika class receiverData ada (mencegah errior)
+if(receiverValue){
+    receiverValue.innerHTML = receiverUrl;
+
+    let messageValue = document.getElementsByClassName('messageData')[0];
+    messageValue.innerHTML = messageUrl;
+    
+    let senderValue = document.getElementsByClassName('senderData')[0];
+    senderValue.innerHTML = senderUrl;
+}
